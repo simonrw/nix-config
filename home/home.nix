ce5544511cf0f6f7553e838f227fbed1798d508a
Simon Walker <s.r.walker101@googlemail.com>
2024-03-18 20:16:34 +0000

    Fix build errors

--- home/home.nix
+++ home/home.nix
@@ -171,7 +171,7 @@ in {
 
   services.gpg-agent = lib.mkIf isLinux {
     enable = true;
-    pinentryFlavor = "gnome3";
+    pinentryPackage = pkgs.pinentry-gnome;
     enableFishIntegration = true;
     enableBashIntegration = true;
   };
