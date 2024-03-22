d872c0a41447d9d40c211856d0d776604d6ce134
Simon Walker <s.r.walker101@googlemail.com>
2024-03-22 09:18:31 +0000

    More post-flake lock update changes

--- home/home.nix
+++ home/home.nix
@@ -171,7 +171,7 @@ in {
 
   services.gpg-agent = lib.mkIf isLinux {
     enable = true;
-    pinentryPackage = pkgs.pinentry-gnome;
+    pinentryPackage = pkgs.pinentry-gnome3;
     enableFishIntegration = true;
     enableBashIntegration = true;
   };
