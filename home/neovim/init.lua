59075186baebbfa78832317711b7fb2ae8b0058c
Simon Walker <s.r.walker101@googlemail.com>
2024-12-20 10:56:13 +0000

    Further configuration of gitsigns and telescope

--- home/neovim/init.lua
+++ home/neovim/init.lua
@@ -1,1 +1,3 @@
+require("config.settings")
 require("config.lazy")
+require("config.keymaps")
--- home/neovim/lua/plugins/lsp.lua
+++ home/neovim/lua/plugins/lsp.lua
@@ -14,6 +14,59 @@ return {
                 }
             }
         },
-        config = function() require("lspconfig").lua_ls.setup {} end
+        config = function()
+            require("lspconfig").lua_ls.setup {}
+            local __binds = {
+                {
+                    action = vim.lsp.buf.type_definition,
+                    key = "gy",
+                    mode = "n",
+                    options = {noremap = true, silent = true}
+                }, {
+                    action = vim.lsp.buf.implementation,
+                    key = "gi",
+                    mode = "n",
+                    options = {noremap = true, silent = true}
+                }, {
+                    action = vim.lsp.buf.hover,
+                    key = "<leader>k",
+                    mode = "n",
+                    options = {noremap = true, silent = true}
+                }, {
+                    action = vim.lsp.buf.rename,
+                    key = "<leader>r",
+                    mode = "n",
+                    options = {noremap = true, silent = true}
+                }, {
+                    action = vim.diagnostic.goto_next,
+                    key = "]d",
+                    mode = "n",
+                    options = {noremap = true, silent = true}
+                }, {
+                    action = vim.diagnostic.goto_prev,
+                    key = "[d",
+                    mode = "n",
+                    options = {noremap = true, silent = true}
+                }, {
+                    action = function()
+                        vim.lsp.buf.code_action({
+                            source = {organizeImports = true}
+                        })
+                    end,
+                    key = "<leader>a",
+                    mode = "n",
+                    options = {noremap = true, silent = true}
+                }, {
+                    action = vim.lsp.buf.signature_help,
+                    key = "<C-h>",
+                    mode = "i",
+                    options = {noremap = true, silent = true}
+                }
+            }
+
+            for _, map in ipairs(__binds) do
+                vim.keymap.set(map.mode, map.key, map.action, map.options)
+            end
+        end
     }
 }
