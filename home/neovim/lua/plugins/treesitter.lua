f02b348a7f72b299e3de38e8910ed99640bd563c
Simon Walker <s.r.walker101@googlemail.com>
2024-12-21 22:20:07 +0000

    Add more treesitter languages

--- home/neovim/lua/plugins/treesitter.lua
+++ home/neovim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = {"lua", "vim", "rust", "python"},
+                ensure_installed = {"lua", "vim", "rust", "python", "nix", "yaml"},
                 sync_install = false,
                 highlight = {enable = true},
                 indent = {enable = true}
