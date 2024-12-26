bed520441a6b153b13c2237ec1bc8b4540d0c935
Simon Walker <s.r.walker101@googlemail.com>
2024-12-26 22:28:16 +0000

    Include telescope fzf native

--- home/neovim/lua/plugins/telescope.lua
+++ home/neovim/lua/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
 {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
-        dependencies = {'nvim-lua/plenary.nvim'},
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        },
         config = function()
             require("telescope").setup({
                 defaults = {
